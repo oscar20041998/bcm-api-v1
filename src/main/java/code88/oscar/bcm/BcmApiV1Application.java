2e258ce254c5934beb872dc152a53bc4afed0728
vothanhduy20041998 <43023726+vothanhduy20041998@users.noreply.github.com>
2020-12-13 01:29:58 +0700

    add check connection api

--- src/main/java/code88/oscar/bcm/BcmApiV1Application.java
+++ src/main/java/code88/oscar/bcm/BcmApiV1Application.java
@@ -2,12 +2,23 @@ package code88.oscar.bcm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@CrossOrigin(origins = "*")
+@RestController
+@RequestMapping(value = "/api")
 public class BcmApiV1Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BcmApiV1Application.class, args);
 	}
 
+	@RequestMapping(value = "/test-connection", method = RequestMethod.GET)
+	public String connection() {
+	    return "CONNECTED";
+	}
 }
